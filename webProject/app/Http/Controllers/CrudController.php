b9d4e166bfbbb9a8a1581d14b9c97dd1d5edbac2
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2023-01-06 13:29:11 +0200

    change register view

--- webProject/app/Http/Controllers/CrudController.php
+++ webProject/app/Http/Controllers/CrudController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class CrudController extends Controller
 {
-
+  public function setUserData(Request $request)
+  {
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+  }
+  public function getUserData()
+  {
+    $user = User::query()->select('name', 'email','mobile')->get();
+    return $user;
+  }
 }
